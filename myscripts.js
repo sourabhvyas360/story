15ff84b942e96649f7768627774b72f10c5bd971
Sourabh <sourabhvyas360@gmail.com>
2023-12-07 12:07:51 +0530

    first commit

---
+++ myscripts.js
@@ -0,0 +1,31 @@
+var arr = [
+    {dp:"https://images.unsplash.com/photo-1564485377539-4af72d1f6a2f?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1562572159-4efc207f5aff?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://images.unsplash.com/photo-1604681630513-69474a4e253f?q=80&w=1527&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1529139574466-a303027c1d8b?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://images.unsplash.com/photo-1524502397800-2eeaad7c3fe5?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1616683693504-3ea7e9ad6fec?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    {dp:"https://images.unsplash.com/photo-1469334031218-e382a71b716b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",story:"https://images.unsplash.com/photo-1533973860717-d49dfd14cf64?q=80&w=1571&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+]
+
+
+var clutter = ""
+
+arr.forEach(function(elem,idx){
+   clutter += `<div class="story">
+   <img id="${idx}" src="${elem.dp}" alt="">
+  </div>`
+});
+
+document.querySelector("#storiyan").innerHTML = clutter;
+
+
+
+
+document.querySelector("#storiyan").addEventListener("click",function(dets){
+    document.querySelector("#full-screen").style.display = "block"
+        
+    document.querySelector("#full-screen").style.backgroundImage = `url(${arr[dets.target.id].story})`
+
+
+    setTimeout(function(){
+        document.querySelector("#full-screen").style.display = "none"
+    },1000)
+});
